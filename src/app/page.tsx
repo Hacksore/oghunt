200001b68e273ef9853189eeb348bb05405fc360
Ross Litzenberger <rosslitzenberger@gmail.com>
2024-08-18 23:57:56 -0500

    Fetching more data from Product Hunt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,16 @@
-const GET_ALL_POSTS = `
-query {
-  posts {
-    totalCount
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
-    nodes {
-      id
-      createdAt
-      url
-      name
-      tagline
-      description
-    }
-  },
-}
-`;
+import LoadMore from "./component/LoadMore";
+import { filterPosts, getAllPost } from "./lib/data";
 
-interface Post {
-  id: string;
-  name: string;
-  url: string;
-  tagline: string;
-  description: string;
-  createdAt: string;
-}
-
-export default async function Home() {
-  const response = (await fetch("https://api.producthunt.com/v2/api/graphql", {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      query: GET_ALL_POSTS,
-    }),
-  }).then((res) => res.json())) as {
-    data: {
-      posts: {
-        nodes: Post[];
-      };
-    };
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    endCursor?: string;
   };
-
-  const posts = response.data.posts.nodes.filter((post) => {
-    // if the name, description or tagline contains "AI", "GPT", "artificial intelligence" or "machine learning" skip it
-    if (
-      post.name.toLowerCase().includes("ai") ||
-      post.tagline.toLowerCase().includes("ai") ||
-      post.description.toLowerCase().includes("ai") ||
-      post.name.toLowerCase().includes("gpt") ||
-      post.tagline.toLowerCase().includes("gpt") ||
-      post.description.toLowerCase().includes("gpt") ||
-      post.name.toLowerCase().includes("artificial intelligence") ||
-      post.tagline.toLowerCase().includes("artificial intelligence") ||
-      post.description.toLowerCase().includes("artificial intelligence") ||
-      post.name.toLowerCase().includes("machine learning") ||
-      post.tagline.toLowerCase().includes("machine learning") ||
-      post.description.toLowerCase().includes("machine learning")
-    ) {
-      return false;
-    }
-
-    return true;
-  });
+}) {
+  const response = await getAllPost(searchParams?.endCursor)
+  const posts = filterPosts(response.data.posts.nodes);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -83,6 +27,7 @@ export default async function Home() {
           </div>
         );
       })}
+      <LoadMore pageInfo={response.data.posts.pageInfo} />
     </main>
   );
 }
