b3f5c212db267bd9ffa44e7b2fc872dfc372a75f
Ross Litzenberger <RossLitzenberger@users.noreply.github.com>
2024-08-19 19:23:40 -0500

    Fetching more data from Product Hunt (#1)

---
+++ src/app/lib/data.ts
@@ -0,0 +1,93 @@
+const GET_ALL_POSTS = `
+query GetAllPosts (
+    $first: Int
+    $last: Int
+    $before: String
+    $after: String
+) {
+    posts (
+        first: $first
+        last: $last
+        before: $before
+        after: $after
+    ) {
+        totalCount
+        pageInfo {
+            hasNextPage
+            endCursor
+        }
+        nodes {
+            id
+            createdAt
+            url
+            name
+            tagline
+            description
+        }
+  },
+}
+`;
+
+export interface Post {
+    id: string;
+    name: string;
+    url: string;
+    tagline: string;
+    description: string;
+    createdAt: string;
+}
+
+export interface PageInfo {
+    hasNextPage: boolean | undefined
+    endCursor: string | undefined
+}
+
+export async function getAllPost(endCursor?: string | null) {
+    return (await fetch("https://api.producthunt.com/v2/api/graphql", {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.API_TOKEN}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+            query: GET_ALL_POSTS,
+            variables: {
+                first: 100,
+                after: endCursor
+            }
+        }),
+    }).then((res) => res.json())) as {
+        data: {
+            posts: {
+                nodes: Post[];
+                pageInfo: PageInfo;
+                totalCount: number;
+            };
+        };
+    };
+};
+
+export const filterPosts = (nodes: Post[]) => {
+    return nodes.filter((post) => {
+        // if the name, description or tagline contains "AI", "GPT", "artificial intelligence" or "machine learning" skip it
+        if (
+            post.name.toLowerCase().includes("ai") ||
+            post.tagline.toLowerCase().includes("ai") ||
+            post.description.toLowerCase().includes("ai") ||
+            post.name.toLowerCase().includes("gpt") ||
+            post.tagline.toLowerCase().includes("gpt") ||
+            post.description.toLowerCase().includes("gpt") ||
+            post.name.toLowerCase().includes("artificial intelligence") ||
+            post.tagline.toLowerCase().includes("artificial intelligence") ||
+            post.description.toLowerCase().includes("artificial intelligence") ||
+            post.name.toLowerCase().includes("machine learning") ||
+            post.tagline.toLowerCase().includes("machine learning") ||
+            post.description.toLowerCase().includes("machine learning")
+        ) {
+            return false;
+        }
+
+        return true;
+    });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,88 +1,21 @@
-const GET_ALL_POSTS = `
-query {
-  posts {
-    totalCount
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
-    nodes {
-      id
-      createdAt
-      url
-      name
-      tagline
-      description
-    }
-  },
-}
-`;
+import PostList from "./component/PostList";
+import { filterPosts, getAllPost } from "./lib/data";
 
-interface Post {
-  id: string;
-  name: string;
-  url: string;
-  tagline: string;
-  description: string;
-  createdAt: string;
-}
-
-export default async function Home() {
-  const response = (await fetch("https://api.producthunt.com/v2/api/graphql", {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
-    },
-    method: "POST",
-    body: JSON.stringify({
-      query: GET_ALL_POSTS,
-    }),
-  }).then((res) => res.json())) as {
-    data: {
-      posts: {
-        nodes: Post[];
-      };
-    };
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    endCursor?: string;
   };
-
-  const posts = response.data.posts.nodes.filter((post) => {
-    // if the name, description or tagline contains "AI", "GPT", "artificial intelligence" or "machine learning" skip it
-    if (
-      post.name.toLowerCase().includes("ai") ||
-      post.tagline.toLowerCase().includes("ai") ||
-      post.description.toLowerCase().includes("ai") ||
-      post.name.toLowerCase().includes("gpt") ||
-      post.tagline.toLowerCase().includes("gpt") ||
-      post.description.toLowerCase().includes("gpt") ||
-      post.name.toLowerCase().includes("artificial intelligence") ||
-      post.tagline.toLowerCase().includes("artificial intelligence") ||
-      post.description.toLowerCase().includes("artificial intelligence") ||
-      post.name.toLowerCase().includes("machine learning") ||
-      post.tagline.toLowerCase().includes("machine learning") ||
-      post.description.toLowerCase().includes("machine learning")
-    ) {
-      return false;
-    }
-
-    return true;
-  });
+}) {
+  const response = await getAllPost(searchParams?.endCursor)
+  const posts = filterPosts(response.data.posts.nodes);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Sites on ProductHunt without AI</h1>
-      {posts.map((post) => {
-        return (
-          <div
-            key={post.id}
-            className="flex flex-col items-center justify-between p-8"
-          >
-            <a href={post.url}><h2 className="text-4xl font-bold">{post.name}</h2></a>
-            <p className="text-lg">{post.tagline}</p>
-            <p className="text-lg">{post.description}</p>
-          </div>
-        );
-      })}
+    <main className="flex min-h-screen flex-col p-24">
+      <h1 className="text-4xl md:text-5xl font-bold pb-20 px-8 bg-gradient-to-r from-pink-300 to-orange-300 bg-clip-text text-transparent">Sites on ProductHunt without AI</h1>
+      <PostList initPosts={posts} initPageInfo={response.data.posts.pageInfo}  />
     </main>
   );
 }
