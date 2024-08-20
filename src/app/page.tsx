78001d6173ee621103d40d9296ebe2252163d9a6
maxdemaio <maxdman321@gmail.com>
2024-08-19 21:25:21 -0400

    lower padding on mobi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({
 
   return (
     <main className="flex min-h-screen flex-col p-4 md:p-24">
-      <h1 className="text-4xl md:text-5xl font-bold pb-20 px-8 bg-gradient-to-r from-pink-300 to-orange-300 bg-clip-text text-transparent">Sites on ProductHunt without AI</h1>
+      <h1 className="text-4xl md:text-5xl font-bold pb-6 md:pb-20 px-8 bg-gradient-to-r from-pink-300 to-orange-300 bg-clip-text text-transparent">Sites on ProductHunt without AI</h1>
       <PostList initPosts={posts} initPageInfo={response.data.posts.pageInfo}  />
     </main>
   );
