8b15174c018d0b537cbbd21a4b0fa092875584e4
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2024-08-20 15:18:59 +1200

    make pill and add date

--- src/app/component/PostList.tsx
+++ src/app/component/PostList.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { format, compareAsc } from "date-fns";
 
 import { PageInfo, Post } from "../lib/data";
 import { useCallback, useRef, useState } from "react";
@@ -10,6 +11,7 @@ export default function PostList({
   initPosts: Post[];
   initPageInfo: PageInfo;
 }) {
+  const date = format(new Date(), "MM/dd/yyyy");
   const [posts, setPosts] = useState<Post[]>(initPosts);
   const pageInfo = useRef(initPageInfo);
 
@@ -24,8 +26,8 @@ export default function PostList({
 
   return (
     <div className="flex flex-col items-center">
-      <div className="flex justify-start w-full pl-8 mb-8">
-        {`${posts.length} products without AI launched on Date`}
+      <div className="flex justify-start w-full pl-8 mb-8 ">
+        <div className="border p-2 rounded-full">{`${posts.length} products without AI launched on ${date}`}</div>
       </div>
       {posts.map((post, index) => {
         return (
