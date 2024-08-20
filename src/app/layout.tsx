5a576560b3a8859273c32194bdcaf8f45b349bc1
JoshHyde9 <joshhyde546@gmail.com>
2024-08-20 12:26:05 +1000

    added product hunt banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import Image from "next/image";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -10,7 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <a href="#">
+          <div className="text-neutral-800 p-2 text-center text-xs md:text-sm flex items-center justify-center bg-gradient-to-r from-pink-300 to-orange-300">
+            <div className="relative h-[24px] w-[24px] md:mr-2">
+              <Image src="/logo-128.png" alt="logo" fill sizes="24px" />
+            </div>
+            We're live on Product Hunt today! Click to show your support!
+          </div>
+        </a>
+        {children}
+      </body>
     </html>
   );
 }
