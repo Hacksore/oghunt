00aa89ae2b84d8731d6a032844d481da8e872fe1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-19 20:27:31 -0500

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "oghunt - Sites on ProductHunt without AI",
-  description: "Sites on ProductHunt without AI",
-};
 
 export default function RootLayout({
   children,
