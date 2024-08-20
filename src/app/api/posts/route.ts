0471ef2fe91051879a486574e063c13fedcff0b7
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-20 18:18:14 -0500

    Async my ass

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const data = fetch(
+    const data = await fetch(
         "https://pub-3db3ed9313c4427fadfa81f0323b18f8.r2.dev/latest.json",
     ).then((res) => res.json());
     return Response.json(data);
