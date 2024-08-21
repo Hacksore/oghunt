243dcf157c02ece3116c7dd8da9abb4acb48ecf3
JoshHyde9 <joshhyde546@gmail.com>
2024-08-21 14:33:58 +1000

    added loading skeleton for page

---
+++ src/app/component/PostSkeleton.tsx
@@ -0,0 +1,12 @@
+export const PostSkeleton = () => {
+  return (
+    <div className="max-w-4xl p-8">
+      <div className="h-5 bg-neutral-700 rounded-full dark:bg-gray-700 w-3/5 mb-6"></div>
+      <div className="h-2 bg-neutral-700 rounded-full dark:bg-gray-700 w-5/12 mb-5"></div>
+      <div className="h-2 bg-neutral-700 rounded-full dark:bg-gray-700 w-4/12 mb-4"></div>
+      <div className="h-2 bg-neutral-700 rounded-full dark:bg-gray-700 w-9/12 mb-4"></div>
+      <div className="h-2 bg-neutral-700 rounded-full dark:bg-gray-700 w-10/12 mb-4"></div>
+      <div className="h-2 bg-neutral-700 rounded-full dark:bg-gray-700 w-7/12"></div>
+    </div>
+  );
+};
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,21 @@
+import { PostSkeleton } from "./component/PostSkeleton";
+
 export default function Loading() {
-    return (
-        <div>
-            <h1>Loading</h1>
+  return (
+    <div className="flex min-h-screen flex-col p-4 md:p-24">
+      <div className="h-10 animate-pulse bg-neutral-700 rounded-full dark:bg-gray-700 ml-8 mb-6 md:mb-20 w-4/5"></div>
+      <div className="flex flex-col items-center animate-pulse">
+        <div className="flex flex-col justify-start gap-2 w-full mt-4 mb-8">
+          <div className="flex flex-col justify-start gap-2 w-full mb-8 ml-8">
+            <div className="h-6 bg-neutral-700 rounded-full dark:bg-gray-700 w-3/5 mb-4"></div>
+            <div className="h-3 bg-neutral-700 rounded-full dark:bg-gray-700 w-2/5 mb-4"></div>
+          </div>
+
+          {new Array(5).fill("").map((_, i) => (
+            <PostSkeleton key={i} />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
