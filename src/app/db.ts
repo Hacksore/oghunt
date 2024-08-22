760918ffb66e088a895b549b9f6d4f944d2fcd0c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-21 20:56:44 -0500

    Scuffed still

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 // Setup
-const connectionString = `${process.env.TURSO_DATABASE_URL}`
-const authToken = `${process.env.TURSO_AUTH_TOKEN}`
+const connectionString = `${process.env.TURSO_DATABASE_URL}`;
+const authToken = `${process.env.TURSO_AUTH_TOKEN}`;
 
 // Init prisma client
 const libsql = createClient({
   url: connectionString,
   authToken,
-})
-const adapter = new PrismaLibSQL(libsql)
-const prisma = new PrismaClient({ adapter })
+});
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
