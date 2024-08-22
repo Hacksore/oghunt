1adbce94040ac312a0f081349fcfd8aee445a971
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-21 20:16:21 -0500

    It's borked

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const prisma = new PrismaClient();
+const { DATABASE_URL, DATABASE_TOKEN } = process.env;
+
+if (!DATABASE_URL || !DATABASE_TOKEN) {
+  throw new Error("DATABASE_URL and DATABASE_TOKEN must be set");
+}
+
+const libsql = createClient({
+  url: DATABASE_URL,
+  authToken: DATABASE_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
