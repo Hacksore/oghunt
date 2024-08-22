70ad81c76ac0ee675cf5796efec13c39b14c2f9d
typesafe <135900154+typesafeui@users.noreply.github.com>
2024-08-22 15:56:11 +0200

    SEO implementation (VERY HARD) (#56)

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://oghunt.com',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 1,
+    }
+  ]
+}
