845435def016cbb4377c1d2748e811006bfd9bfe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-22 19:28:15 -0500

    Ligmatures

--- src/app/api/update-posts/route.ts
+++ src/app/api/update-posts/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
   if (
     authHeader !== `Bearer ${process.env.CRON_SECRET}` &&
-    process.env.NODE_ENV !== "production"
+    process.env.NODE_ENV === "production"
   ) {
     return new Response("Unauthorized", {
       status: 401,
