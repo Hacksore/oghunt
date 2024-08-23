420aad7453c863f0d5974c7c43678abc86c12ec2
BostonRohan <bostonrohan@gmail.com>
2024-08-23 14:35:54 -0400

    fix: add type to prop

--- src/app/component/icons/ChevronUp.tsx
+++ src/app/component/icons/ChevronUp.tsx
@@ -1,6 +1,6 @@
 type ChevronUpProps = { className: string };
 
-export const ChevronUp = ({ className }) => (
+export const ChevronUp = ({ className }: ChevronUpProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="20"
