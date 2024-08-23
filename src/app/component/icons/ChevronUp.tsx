873717ebcf70c39014b797a9c4677ced26ca56fa
BostonRohan <bostonrohan@gmail.com>
2024-08-23 14:30:34 -0400

    fix: explicity add prop & adjust dimensions

--- src/app/component/icons/ChevronUp.tsx
+++ src/app/component/icons/ChevronUp.tsx
@@ -1,15 +1,17 @@
-export const ChevronUp = (props) => (
+type ChevronUpProps = { className: string };
+
+export const ChevronUp = ({ className }) => (
   <svg
-    {...props}
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
+    className={className}
   >
     <path d="m18 15-6-6-6 6" />
   </svg>
