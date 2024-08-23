bdcf893224970e343db1a788e818010155a2e9bb
typesafe <135900154+typesafeui@users.noreply.github.com>
2024-08-23 23:34:48 +0200

    Performance improvement and schema fix 🗣️ (#62)

--- src/app/lib/persistence.ts
+++ src/app/lib/persistence.ts
@@ -4,9 +4,11 @@ import {
 } from "./data";
 import db from "../db";
 import { hasAi } from "../utils/string";
+import { Post as PostType } from "../types";
+import { Prisma } from "@prisma/client";
 
 export async function getTodaysLaunches() {
-  const posts = await db.post.findMany({
+  const posts = (await db.post.findMany({
     where: {
       // only get the posts that are the same day as today
       createdAt: {
@@ -15,75 +17,129 @@ export async function getTodaysLaunches() {
       },
     },
     include: {
-      topics: true,
+      topics: {
+        select: {
+          Topic: true
+        }
+      }
     },
-  });
+  })).map(post => ({
+    ...post,
+    topics: post.topics.map(topic => topic.Topic)
+  }));
 
   return posts.sort((a, b) => b.votesCount - a.votesCount);
 }
 
+function generateDBPost(post: PostType): Prisma.PostCreateManyInput {
+  return {
+    id: post.id,
+    votesCount: post.votesCount,
+    name: post.name,
+    description: post.description,
+    tagline: post.tagline,
+    url: post.url,
+    hasAi: hasAi(convertPostToProductPost(post)),
+    thumbnailUrl: post.thumbnail.url,
+  }
+}
+
 // NOTE: this will get all the posts and persist the data to the database
 export async function fetchAndUpdateDatabase() {
+  const MAX_PARTITION_SIZE = 50;
+  const MAX_CONCURRENCY = 5;
+
+  const postsToCreate = [];
+  const postsToUpdate: PostType[] = [];
+  const partitioned_create_posts = [];
+
   const posts = await getAllDailyPostRightNow();
 
-  // TODO: we can use lodash to make this more efficient and only get changed/new items
+  const existingPosts = (await db.post.findMany({
+    select: {
+      id: true
+    }
+  })).reduce((acc, post) => {
+    acc[post.id] = true;
+    return acc;
+  }, {
+
+  } as {
+    [key: string]: boolean;
+  })
 
   // TODO: handle errors?
-  await Promise.allSettled(
-    posts.map((post) =>
-      db.post.upsert({
+
+  const allTopics = posts.flatMap(post => post.topics.nodes);
+
+
+
+
+
+  for (const post of posts) {
+    if (existingPosts[post.id]) postsToUpdate.push(post);
+    else postsToCreate.push(post)
+  }
+
+
+
+  while (postsToCreate.length) {
+    partitioned_create_posts.push(postsToCreate.splice(0, MAX_PARTITION_SIZE));
+  }
+
+
+  while (partitioned_create_posts.length) {
+    const toAdd = partitioned_create_posts.pop();
+    if (!toAdd) break;
+    await db.post.createMany({
+      data: toAdd.map(generateDBPost),
+      skipDuplicates: true
+    })
+  }
+
+
+
+
+  await db.topic.createMany({
+    data: allTopics,
+    skipDuplicates: true,
+  })
+
+
+  const promises = [];
+
+  async function runPostUpdateQueue() {
+    while (postsToUpdate.length) {
+      const post = postsToUpdate.pop();
+      if (!post) break;
+      await db.post.update({
         where: {
-          id: post.id,
+          id: post.id
         },
-        update: {
-          votesCount: post.votesCount,
-          name: post.name,
-          description: post.description,
-          tagline: post.tagline,
-          url: post.url,
-          thumbnailUrl: post.thumbnail.url,
-          topics: {
-            connectOrCreate: post.topics.nodes.map(
-              ({ id, description, name }) => ({
-                where: {
-                  id,
-                },
-                create: {
-                  id,
-                  description,
-                  name,
-                },
-              }),
-            ),
-          },
-        },
-        create: {
-          id: post.id,
-          votesCount: post.votesCount,
-          name: post.name,
-          description: post.description,
-          tagline: post.tagline,
-          url: post.url,
-          hasAi: hasAi(convertPostToProductPost(post)),
-          thumbnailUrl: post.thumbnail.url,
-          topics: {
-            connectOrCreate: post.topics.nodes.map(
-              ({ id, description, name }) => ({
-                where: {
-                  id,
-                },
-                create: {
-                  id,
-                  description,
-                  name,
-                },
-              }),
-            ),
-          },
-        },
-      }),
-    ),
-  );
+        data: generateDBPost(post)
+      });
+    }
+
+  }
+
+
+  for (let i = 0; i < MAX_CONCURRENCY; ++i) promises.push(runPostUpdateQueue());
+
+
+  await Promise.all(promises);
+
+
+  const topicPosts = posts.flatMap(post => post.topics.nodes.map(topic => ({
+    postId: post.id,
+    topicId: topic.id
+  })));
+
+
+  await db.topicPost.createMany({
+    data: topicPosts,
+    skipDuplicates: true
+  })
+
 
   return posts;
 }
