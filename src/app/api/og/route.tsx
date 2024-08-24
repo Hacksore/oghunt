66fbdff1f9385c235d9eef469bcf8e534c214d25
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-24 14:13:19 -0500

    Ayo 2

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,12 +5,10 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 // Image metadata
-export const size = {
+const size = {
   width: 1200,
   height: 630,
 };
-
-export const contentType = "image/png";
 
 const API_URL =
   process.env.NODE_ENV === "production" ? "https://oghunt.com" : "http://localhost:3000";
