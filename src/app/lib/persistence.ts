c6b4989934cc7b527cd591903fc0eeded61f9108
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-23 19:31:25 -0500

    Format

--- src/app/lib/persistence.ts
+++ src/app/lib/persistence.ts
@@ -8,24 +8,26 @@ import { Post as PostType } from "../types";
 import { Prisma } from "@prisma/client";
 
 export async function getTodaysLaunches() {
-  const posts = (await db.post.findMany({
-    where: {
-      // only get the posts that are the same day as today
-      createdAt: {
-        gte: new Date(new Date().setHours(0, 0, 0, 0)),
-        lt: new Date(new Date().setHours(23, 59, 59, 999)),
+  const posts = (
+    await db.post.findMany({
+      where: {
+        // only get the posts that are the same day as today
+        createdAt: {
+          gte: new Date(new Date().setHours(0, 0, 0, 0)),
+          lt: new Date(new Date().setHours(23, 59, 59, 999)),
+        },
       },
-    },
-    include: {
-      topics: {
-        select: {
-          Topic: true
-        }
-      }
-    },
-  })).map(post => ({
+      include: {
+        topics: {
+          select: {
+            Topic: true,
+          },
+        },
+      },
+    })
+  ).map((post) => ({
     ...post,
-    topics: post.topics.map(topic => topic.Topic)
+    topics: post.topics.map((topic) => topic.Topic),
   }));
 
   return posts.sort((a, b) => b.votesCount - a.votesCount);
@@ -41,7 +43,7 @@ function generateDBPost(post: PostType): Prisma.PostCreateManyInput {
     url: post.url,
     hasAi: hasAi(convertPostToProductPost(post)),
     thumbnailUrl: post.thumbnail.url,
-  }
+  };
 }
 
 // NOTE: this will get all the posts and persist the data to the database
@@ -55,56 +57,47 @@ export async function fetchAndUpdateDatabase() {
 
   const posts = await getAllDailyPostRightNow();
 
-  const existingPosts = (await db.post.findMany({
-    select: {
-      id: true
-    }
-  })).reduce((acc, post) => {
-    acc[post.id] = true;
-    return acc;
-  }, {
-
-  } as {
-    [key: string]: boolean;
-  })
+  const existingPosts = (
+    await db.post.findMany({
+      select: {
+        id: true,
+      },
+    })
+  ).reduce(
+    (acc, post) => {
+      acc[post.id] = true;
+      return acc;
+    },
+    {} as {
+      [key: string]: boolean;
+    },
+  );
 
   // TODO: handle errors?
-
-  const allTopics = posts.flatMap(post => post.topics.nodes);
-
-
-
-
+  const allTopics = posts.flatMap((post) => post.topics.nodes);
 
   for (const post of posts) {
     if (existingPosts[post.id]) postsToUpdate.push(post);
-    else postsToCreate.push(post)
+    else postsToCreate.push(post);
   }
-
-
 
   while (postsToCreate.length) {
     partitioned_create_posts.push(postsToCreate.splice(0, MAX_PARTITION_SIZE));
   }
-
 
   while (partitioned_create_posts.length) {
     const toAdd = partitioned_create_posts.pop();
     if (!toAdd) break;
     await db.post.createMany({
       data: toAdd.map(generateDBPost),
-      skipDuplicates: true
-    })
+      skipDuplicates: true,
+    });
   }
-
-
-
 
   await db.topic.createMany({
     data: allTopics,
     skipDuplicates: true,
-  })
-
+  });
 
   const promises = [];
 
@@ -114,32 +107,28 @@ export async function fetchAndUpdateDatabase() {
       if (!post) break;
       await db.post.update({
         where: {
-          id: post.id
+          id: post.id,
         },
-        data: generateDBPost(post)
+        data: generateDBPost(post),
       });
     }
-
   }
-
 
   for (let i = 0; i < MAX_CONCURRENCY; ++i) promises.push(runPostUpdateQueue());
 
-
   await Promise.all(promises);
 
-
-  const topicPosts = posts.flatMap(post => post.topics.nodes.map(topic => ({
-    postId: post.id,
-    topicId: topic.id
-  })));
-
+  const topicPosts = posts.flatMap((post) =>
+    post.topics.nodes.map((topic) => ({
+      postId: post.id,
+      topicId: topic.id,
+    })),
+  );
 
   await db.topicPost.createMany({
     data: topicPosts,
-    skipDuplicates: true
-  })
-
+    skipDuplicates: true,
+  });
 
   return posts;
 }
