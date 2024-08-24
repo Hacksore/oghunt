647a2c935661050738e7b8037bb92d02803f63b6
typesafeui <typesafeui@gmail.com>
2024-08-24 23:29:48 +0200

    fixing undefined value cached

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -4,9 +4,13 @@ import { filterPosts } from "@/app/utils/string";
 
 export const dynamic = "force-dynamic";
 
-const getTodaysLaunchesCached = unstable_cache(() => getTodaysLaunches(), ["todaylaunches"], {
-  revalidate: 900, // 15 minutes
-});
+async function getTodaysLaunchesCached() {
+  const cachedVal = await unstable_cache(() => getTodaysLaunches(), ["todaylaunches"], {
+    revalidate: 900, // 15 minutes
+  })();
+  if (!cachedVal) return await getTodaysLaunches();
+  return cachedVal;
+}
 
 export async function GET() {
   const allPosts = await getTodaysLaunchesCached();
