04de7df784d91cf6cbc5d696e3d32a0681855cff
typesafe <135900154+typesafeui@users.noreply.github.com>
2024-08-27 20:10:49 +0200

    W OG 🥶 (#110)

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,92 +3,57 @@ import type { Metadata } from "next";
 export const OG_URL =
   process.env.NODE_ENV !== "production" ? "http://localhost:3000" : "https://oghunt.com";
 
-export const tagline = "Product Hunt with ZERO AI Slop™";
+type OpenGraphFunction = () => Promise<Metadata>;
 
-// TODO: this should just programmatically update when the data is refetched
-const OG_CACHE_KEY = "v3";
-
-export const baseMetadata: Metadata = {
-  metadataBase: new URL(OG_URL),
-  title: {
-    default: "OGHUNT",
-    template: "%s",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  description: tagline,
-  openGraph: {
-    title: "OGHUNT",
-    description: tagline,
-    siteName: "OGHUNT",
-    images: [
-      {
-        url: `${OG_URL}/api/og?c=${OG_CACHE_KEY}`,
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    title: "OGHUNT",
-    card: "summary_large_image",
-    images: [
-      {
-        url: `${OG_URL}/api/og?c=${OG_CACHE_KEY}`,
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  icons: {
-    shortcut: "/favicon.ico",
-  },
-};
-
-const buildMeta = ({
-  ogImageUrl,
-  description,
-  title,
-}: {
-  ogImageUrl: string;
-  description?: string;
-  title?: string;
-}): Metadata => {
-  baseMetadata.openGraph!.images = ogImageUrl;
-  baseMetadata.twitter!.images = ogImageUrl;
-
-  if (description) {
-    baseMetadata.description = description;
-    baseMetadata.twitter!.description = description;
-    baseMetadata.openGraph!.description = description;
-  }
-
-  if (title) {
-    baseMetadata.title = title;
-    baseMetadata.twitter!.title = title;
-    baseMetadata.openGraph!.title = title;
-  }
-
-  return baseMetadata;
-};
-
-/** Helper to build opengraph metadata with defaults, you should call this in generateMetadata() next function */
-export const createMeta = ({
+export function generateOGHuntMetadata({
   title,
   description,
 }: {
-  title?: string;
+  title: string;
   description?: string;
-}): Metadata => {
-  return buildMeta({
-    ogImageUrl: `${OG_URL}/api/og?c=${OG_CACHE_KEY}`,
-    title,
-    description,
-  });
-};
+}): OpenGraphFunction {
+  return async () => {
+    const CACHE_DELAY = 30 * 1000;
+    const CACHE_INTERVAL = 15 * 60 * 1000;
 
-export default baseMetadata;
+    const cacheKey = Math.floor((Date.now() - CACHE_DELAY) / CACHE_INTERVAL).toString(16);
+
+    return {
+      metadataBase: new URL(OG_URL),
+      title,
+      robots: {
+        index: true,
+        follow: true,
+      },
+      description: description,
+      openGraph: {
+        title,
+        description: description,
+        siteName: "OGHUNT",
+        images: [
+          {
+            url: `${OG_URL}/api/og/${cacheKey}.png`,
+            width: 1200,
+            height: 630,
+          },
+        ],
+        locale: "en-US",
+        type: "website",
+      },
+      twitter: {
+        title: "OGHUNT",
+        card: "summary_large_image",
+        images: [
+          {
+            url: `${OG_URL}/api/og/${cacheKey}.png`,
+            width: 1200,
+            height: 630,
+          },
+        ],
+      },
+      icons: {
+        shortcut: "/favicon.ico",
+      },
+    };
+  };
+}
