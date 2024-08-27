16160036d0ce5bec858605e9c2f316a057788709
Josh Hyde <40751087+JoshHyde9@users.noreply.github.com>
2024-08-27 21:18:29 +1000

    Fix loading skeletons (#106)

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,20 +2,34 @@ import { PostSkeleton } from "./component/PostSkeleton";
 
 export default function Loading() {
   return (
-    <div className="flex min-h-screen flex-col p-4 md:p-24">
-      <div className="mb-6 ml-8 h-10 w-4/5 animate-pulse rounded-full bg-neutral-700 md:mb-20 dark:bg-gray-700"></div>
-      <div className="flex animate-pulse flex-col items-center">
-        <div className="mb-8 mt-4 flex w-full flex-col justify-start gap-2">
-          <div className="mb-8 ml-8 flex w-full flex-col justify-start gap-2">
-            <div className="mb-4 h-6 w-3/5 rounded-full bg-neutral-700 dark:bg-gray-700"></div>
-            <div className="mb-4 h-3 w-2/5 rounded-full bg-neutral-700 dark:bg-gray-700"></div>
-          </div>
+    <main className="flex min-h-screen w-full animate-pulse flex-col items-center px-4 pt-10 md:px-8">
+      <header className="flex w-full flex-col items-center gap-4 pb-10">
+        <div className="mr-auto h-10 w-10/12 rounded-full bg-neutral-700 md:mb-4 lg:m-0 lg:mb-4 lg:w-8/12 2xl:w-2/6 dark:bg-gray-700"></div>
+        <div className="mb-4 mr-auto h-10 w-7/12 rounded-full bg-neutral-700 md:hidden lg:m-0 dark:bg-gray-700"></div>
 
+        <div className="mb-4 h-[50px] w-[300px] rounded-full bg-neutral-700 dark:bg-gray-700"></div>
+
+        <div className="w-full md:w-11/12 lg:w-8/12 2xl:w-2/6">
+          <div className="mb-4 h-5 w-3/12 rounded-full bg-neutral-700 dark:bg-gray-700"></div>
+          <div className="w-full">
+            <div className="mb-4 h-[32px] rounded-full bg-neutral-700 dark:bg-gray-700"></div>
+          </div>
+          <div className="mb-4 h-3 w-2/5 rounded-full bg-neutral-700 dark:bg-gray-700"></div>
+        </div>
+      </header>
+
+      <div className="w-full md:w-11/12 2xl:w-[1129.65px]">
+        <div className="flex flex-col gap-10 overflow-hidden md:gap-4">
           {new Array(5).fill("").map((_, i) => (
             <PostSkeleton key={i} />
           ))}
         </div>
+
+        <div className="flex flex-col items-center pt-8">
+          <div className="mb-4 h-4 w-11/12 rounded-full bg-neutral-700 md:w-5/12 dark:bg-gray-700"></div>
+          <div className="mb-4 h-2 w-11/12 rounded-full bg-neutral-700 md:w-4/12 dark:bg-gray-700"></div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
