adeed46ce31d831dbbea6df3c31ce2dc3e6e9e2c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-28 07:45:54 -0500

    Add new flag

--- src/app/api/check/[slug]/route.tsx
+++ src/app/api/check/[slug]/route.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/app/db";
+import { getStartAndEndOfDayInUTC } from "@/app/utils/date";
 
-export async function GET(request: Request, { params }: { params: { slug: string } }) {
+export async function GET(_request: Request, { params }: { params: { slug: string } }) {
   const slug = decodeURIComponent(params.slug);
 
   const post = await prisma.post.findFirst({
@@ -18,5 +19,12 @@ export async function GET(request: Request, { params }: { params: { slug: string
     });
   }
 
-  return Response.json(post);
+  const { postedBefore, postedAfter } = getStartAndEndOfDayInUTC();
+  const wouldShowToday =
+    post.createdAt > new Date(postedAfter) && post.createdAt < new Date(postedBefore);
+
+  return Response.json({
+    ...post,
+    wouldShowToday,
+  });
 }
