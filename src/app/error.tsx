c00484daef777816e2e5af1bb410ef74532a6125
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-16 10:14:42 -0500

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <div className="flex flex-col items-center mx-auto text-2xl font-bold gap-4">
+      <h1>We ran out of money for the DB, please join the discord and fund us</h1>
+      <a href="https://discord.com/servers/trash-devs-796594544980000808">Trash Devs Discord</a>
+    </div>
+  );
+}
