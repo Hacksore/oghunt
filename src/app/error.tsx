627a88d19aba68296bd9b39666a560bcc440011f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-16 10:22:45 -0500

    Make pretty

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,8 +2,11 @@
 export default function Error() {
   return (
     <div className="flex flex-col items-center mx-auto text-2xl font-bold gap-4">
-      <h1>We ran out of money for the DB, please join the discord and fund us</h1>
-      <a href="https://discord.com/servers/trash-devs-796594544980000808">Trash Devs Discord</a>
+      <h1>We ran out of money for the DB, please join the discord and fund us 🙏</h1>
+      <a
+        className="border-b border-[var(--background-start-rgb)] bg-gradient-to-r from-pink-400 to-orange-400 bg-clip-text pt-2 text-xl text-transparent duration-150 hover:border-black dark:border-black dark:from-pink-300 dark:to-orange-300 dark:hover:border-white"
+        href="https://discord.com/servers/trash-devs-796594544980000808">Trash Devs Discord</a>
+      <img src="https://media1.tenor.com/m/dHVat9e2S38AAAAC/rat-cry-mouse-cutie.gif" />
     </div>
   );
 }
