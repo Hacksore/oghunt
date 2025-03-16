281e267ddb9dafac18b335fdbee3c5c19c44a727
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-03-16 09:06:47 -0500

    Cache it up

--- src/app/api/list/route.tsx
+++ src/app/api/list/route.tsx
@@ -1,8 +1,11 @@
 import { getTodaysLaunches } from "@/app/lib/persistence";
 import { filterPosts } from "@/app/utils/string";
+import { unstable_cache } from "next/cache";
 
 export async function GET() {
-  const allPosts = await getTodaysLaunches();
+  const allPosts = await unstable_cache(() => getTodaysLaunches(), ["api-today-launches"], {
+    revalidate: 3600,
+  })();
   const posts = filterPosts(allPosts);
 
   return Response.json(posts);
