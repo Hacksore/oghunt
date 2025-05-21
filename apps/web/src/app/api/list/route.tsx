ee29f165147288c9cae7530281c0fdc762a8c919
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-21 08:40:43 -0500

    Comment and change cache duration

--- apps/web/src/app/api/list/route.tsx
+++ apps/web/src/app/api/list/route.tsx
@@ -2,9 +2,12 @@ import { getTodaysLaunches } from "@/app/lib/persistence";
 import { filterPosts } from "@/app/utils/string";
 import { unstable_cache } from "next/cache";
 
+// NOTE: this function will try to cache the result for 15 minutes
+// however, we can revalidate it from both the update-vote-count and ingest-posts routes
 export async function GET() {
   const allPosts = await unstable_cache(() => getTodaysLaunches(), ["api-today-launches"], {
-    revalidate: 3600,
+    // revalidate every 15 minutes
+    revalidate: 900,
   })();
   const posts = await filterPosts(allPosts);
 
