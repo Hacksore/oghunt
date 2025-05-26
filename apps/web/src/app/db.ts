e85fddd0d4a97cc24e1c78b2672390c5f7bdcecb
Josh <joshhyde546@gmail.com>
2025-05-26 14:35:21 +1000

    fix: stop prisma from creating new instances

--- apps/web/src/app/db.ts
+++ apps/web/src/app/db.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
