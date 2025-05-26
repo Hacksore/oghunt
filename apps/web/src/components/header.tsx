3918201d1ac071907d58dfb9286d74ba3eaeddd4
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-25 23:14:19 -0500

    Fixie wixie

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,12 +1,23 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
+const themes = ["system", "light", "dark"] as const;
+
 export function Header() {
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <header className="w-full">
@@ -15,14 +26,33 @@ export function Header() {
           <Image src="/icon.svg" alt="Oghunt Logo" width={32} height={32} className="h-8 w-8" />
           <span className="text-xl font-semibold">oghunt</span>
         </Link>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="rounded-full"
-        >
-          {theme === "dark" ? <Sun /> : <Moon />}
-        </Button>
+        <div className={cn("flex items-center rounded-full border p-0.5")}>
+          {themes.map((t) => {
+            const isActive = theme === t;
+            return (
+              <Button
+                key={t}
+                type="button"
+                variant={isActive ? "default" : "ghost"}
+                onClick={() => setTheme(t)}
+              >
+                {t === "system" && "system"}
+                {t === "light" && (
+                  <Sun
+                    aria-hidden="true"
+                    className={cn("size-4", isActive && "fill-black dark:fill-white")}
+                  />
+                )}
+                {t === "dark" && (
+                  <Moon
+                    aria-hidden="true"
+                    className={cn("size-4", isActive && "fill-black dark:fill-white")}
+                  />
+                )}
+              </Button>
+            );
+          })}
+        </div>
       </div>
     </header>
   );
