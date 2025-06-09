129163b12a73e2eaad63d476e2da93e167747e09
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-06-09 08:12:25 -0500

    Fix badge

--- apps/web/src/components/peerlist-badge.tsx
+++ apps/web/src/components/peerlist-badge.tsx
@@ -5,13 +5,18 @@ import { Link } from "./link";
 export const PeerlistBadge = () => {
   const { theme, systemTheme } = useTheme();
 
-  // get light or dark mode but also respect system theme
-  const determinedTheme = theme === "system" ? systemTheme : theme;
+  const determineTheme = () => {
+    if (theme === "system") {
+      return systemTheme === "dark" ? "dark" : "light";
+    }
+
+    return theme === "dark" ? "dark" : "light";
+  };
 
   return (
     <Link href="https://peerlist.io/hacksore/project/oghunt-20">
       <img
-        src={`/peerlist-${determinedTheme}.svg`}
+        src={`/peerlist-${determineTheme()}.svg`}
         alt="Peerlist Launch Badge"
         className="mx-auto mb-8 w-42 h-auto"
       />
